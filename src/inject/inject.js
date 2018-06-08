f845be1e44b2fd98487a9f4a5ef1a00e64915bb9
Kai Neuwerth <kai@webchimp.de>
2018-06-08 21:02:23 +0200

    remove console log

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -278,7 +278,6 @@ oPageLiner.addHelpLineToDOM = function (posX, posY, sColor, iHelplineIndex) {
 
 oPageLiner.addHelpLineWithShortcuts = function (e) {
     if (!e.altKey || e.ctrlKey || e.shiftKey) {
-        console.log('aborting...')
         return;
     }
 
